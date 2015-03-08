5726577cc4ab20cf427e5f4a325220a72196f70b
larry <larry@laurodelacruz.com>
2015-03-09 03:54:05 +0800

    Changed legend symbol from pch=151 to lty=c(1,1,1)

--- plot4.R
+++ plot4.R
@@ -28,6 +28,6 @@ plot(data$DateTime, data$Sub_metering_1, type="n", xlab="", ylab="Energy sub met
 lines(data$DateTime, data$Sub_metering_1, col="black")
 lines(data$DateTime, data$Sub_metering_2, col="red")
 lines(data$DateTime, data$Sub_metering_3, col="blue")
-legend("topright", pch=151, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n")
+legend("topright", lty=c(1,1,1), col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n")
 plot(data$DateTime, data$Global_reactive_power, "l", xlab="datetime", ylab="Global_reactive_power")
 dev.off()
